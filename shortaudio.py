6eb36206a012f2441df72ab3abb9bbe4b330e1a4
guti10501 <afgutierrez4@gmail.com>
2023-08-20 12:06:04 +0900

    Adding voice descriptions as comments

--- shortaudio.py
+++ shortaudio.py
@@ -14,11 +14,11 @@ def combine(hostd=[], guestd=[]):
     hostdl = []
     guestdl = []
     for i in range(len(hostd)):
-        voice(hostd[i],'Stephen',f'./output/que{i+1}.mp3')
+        voice(hostd[i],'Stephen',f'./output/que{i+1}.mp3') #Voice of the host
         hostdl.append(AudioSegment.from_file(f'./output/que{i+1}.mp3', format="mp3") + 8)
         os.remove(f'./output/que{i+1}.mp3')
     for i in range(len(guestd)):
-        voice(guestd[i],'Matthew',f'./output/ans{i+1}.mp3')
+        voice(guestd[i],'Matthew',f'./output/ans{i+1}.mp3') #Voice of the guest: Matthew (20s), Joey (40s), Stephen (30s) Joanna (30s), Ruth (20s), Kendra (40s)
         guestdl.append(AudioSegment.from_file(f'./output/ans{i+1}.mp3', format="mp3") + 8)
         os.remove(f'./output/ans{i+1}.mp3')
     silence_duration = 1000
