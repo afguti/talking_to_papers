8227985b1d5e0b018584e4d7b277f35f8267f3ed
guti10501 <afgutierrez4@gmail.com>
2023-08-20 12:05:35 +0900

    Last version that worked well. Just a small fix to complete the conversation is needed

--- generate.py
+++ generate.py
@@ -114,7 +114,7 @@ vectordb = Chroma.from_documents(
 print(f"vectordb created with a collection count of {vectordb._collection.count()}")
 
 #Question answering
-llm_name = "gpt-3.5-turbo-0613"
+llm_name = "gpt-3.5-turbo-16k-0613" ##LLM model to ask the vector database
 from langchain.chat_models import ChatOpenAI
 llm = ChatOpenAI(model_name=llm_name, temperature=0)
 from langchain.chains import RetrievalQA
@@ -129,11 +129,11 @@ paper = next(search.results())
 if not(os.path.exists('./output/topics.txt')):
     prGreen("\nTopics to drive the conversation:")
     text = f'Generate a list of 10 engaging topics based on the provided TEXT that could facilitate a conversation between \
-    the author of the TEXT and an individual without a science background. This person is seeking to comprehend the implications \
-    of the TEXT\'s content on an average person\'s perspective. Organize the topics in a coherent sequence that aids in the \
-    reader\'s understanding of the TEXT. Ensure the topics do not overlap to prevent redundancy. \
-    Formulate topics that are directly related to the content of the TEXT. Print out only the list of topics. \
-    Here is the TEXT for reference:\n{paper.summary}'
+the author of the TEXT and an individual without a science background. This person is seeking to comprehend the implications \
+of the TEXT\'s content on an average person\'s perspective. Organize the topics in a coherent sequence that aids in the \
+reader\'s understanding of the TEXT. Ensure the topics do not overlap to prevent redundancy. \
+Formulate topics that are directly related to the content of the TEXT. Print out only the list of topics. \
+Here is the TEXT for reference:\n{paper.summary}'
     messages =  [  
     {'role':'system', 
      'content':"""You are a popular science communicatora and content creator"""},    
@@ -159,11 +159,11 @@ paper = next(search.results())
 if not(os.path.exists('./output/description.txt')):
     prGreen("\nGenerating the description:")
     text = f'Generate a description of about 100 words based on the provided TEXT below. \
-    The description is for a podcast episode in which there will be a discussion about TEXT. \
-    The description has to be engaging and trigger the curiosity of the reader.\
-    The description has to include at the bottom: LINK TO THE PAPER:\nhttps://arxiv.org/pdf/{paper_id}.pdf \
-    Write a very short (three to four words) but provoking title for the podcast below the LINK, present it as TITLE: \
-    Here is the TEXT for reference:\n{paper.summary}'
+The description is for a podcast episode in which there will be a discussion about TEXT. \
+The description has to be engaging and trigger the curiosity of the reader.\
+The description has to include at the bottom: LINK TO THE PAPER:\nhttps://arxiv.org/pdf/{paper_id}.pdf \
+Write a very short (three to four words) but provoking title for the podcast below the LINK, present it as TITLE: \
+Here is the TEXT for reference:\n{paper.summary}'
     messages =  [  
     {'role':'system', 
      'content':"""You a professional writer and a content creator."""},    
@@ -180,10 +180,10 @@ else:
 if not(os.path.exists('./output/final_p1.mp3')):
     prGreen("\nThe introduction:")
     text = f'Based on the TEXT below, print out an engaging introduction for an episode of a podcast named "The PaperCast". \
-    The introduction MUST BE MENTION that the content is created by Artificial Intelligence. The introduction has to mention \
-    that we will discuss about a particular paper titled {paper.title}. Provide the main topic and a summary of the paper \
-    in simple terms for popular science communication. Mention that the link to the paper is in the description of the episode. \
-    The name of the host is "Santiago".\nTEXT:\n{paper.summary}'
+The introduction MUST BE MENTION that the content is created by Artificial Intelligence. The introduction has to mention \
+that we will discuss about a particular paper titled {paper.title}. Provide the main topic and a summary of the paper \
+in simple terms for popular science communication. Mention that the link to the paper is in the description of the episode. \
+The name of the host is "Santiago".\nTEXT:\n{paper.summary}'
     messages =  [  
     {'role':'system', 
      'content':"""You are a popular science communicator"""},    
@@ -206,23 +206,79 @@ if not(os.path.exists('./output/Conversation.mp3')):
         prGreen("\nQuicking off the conversation:")
         print(f"Authors are: {paper.authors}")
         author = input("Author: ")
-        firsq = query(f'Based on the content, print out the beginning of an engaign conversation between a podcast \
-        host and a guest about {qq[0]}. Introduce the guest named {author} by its name and mention that they is one \
-        of the authors of the paper we will talk about. The conversation has to be easy to understand for popular science communication. \
-        You will use "Host: " and "Guest: " at the beginning of each line of the conversation. \
-        Start the conversation with "Today, we have an special guest" \
-        This part is the continuation after an introduction. Do not end the conversation at the end. AVOID LISTING NAMES.',1)
-        save_output(f"Conversation.txt", f'{firsq}\n\n   ####   DIVISION   ####\n\n')
+        #firsq = query(f'Based on the content, print out the beginning of an engaging conversation between a podcast \
+        #host and a guest about {qq[0]}. Introduce the guest named {author} by its name and mention that they is one \
+        #of the authors of the paper titled {paper.title}. The conversation has to be easy to understand for popular science communication. \
+        #You will use "Host: " and "Guest: " at the beginning of each line of the conversation. \
+        #Start the conversation with "Today, we have an special guest" \
+        #This part is the continuation after an introduction. End the beginning of this conversation with the host \
+        #mentioning his interest to talk about {qq[1]} moving forward. AVOID LISTING NAMES.',0)
+
+        #firsq = query(f'Generate the initial part of an engaging conversation between a podcast host and a guest regarding {qq[0]}. \
+        #Introduce the guest, {author}, as one of the authors of the paper titled {paper.title}. Ensure that the conversation is presented \
+        #in a manner accessible to a popular science audience. You can use "Host: " and "Guest: " labels to indicate the speakers. \
+        #Begin the dialogue with "Today, we are joined by a special guest {author}, who is author of toda\'s paper titled {paper.title}." \
+        #This segment follows a brief introduction, and it should conclude with the host expressing their enthusiasm to further \
+        #discuss {qq[1]} in this conversation. Avoid explicitly listing any names.',0)
+
+        firsq = query(f'Imagine you\'re hosting a podcast episode focused on explaining scientific concepts to a broad audience. \
+The topic of this segment is {qq[0]}. You\'re starting a conversation with an expert guest named {author} to explore specific \
+aspects related to {qq[0]}. Start the conversation with "Today we have {author} who is author of today\'s paper titled {paper.title}". \
+In this segment, dive into insights and information that will be \
+engaging and comprehensible for your listeners. Remember not to finalize the conversation, as it\'s \
+just the beginning of the episode. Please provide a concise conversation about {qq[0]} that includes at least two \
+interventions from both you as the host and your guest, all while maintaining \
+a popular science communication style. You can use "Host: " and "Guest: " labels to indicate the speakers. \
+Conclude this segment by expressing the host\'s eagerness to delve deeper into {qq[1]} in the upcoming part of the conversation.',0)
+
+        save_output(f"Conversation.txt", f'{firsq}\n\n   ####   DIVISION 0  ####\n\n')
         for i in range(len(qq) - 1): 
             prGreen(f"topic {i + 1}: {qq[i + 1]}")
-            conv = query(f'Based on the content, print out a segment of an engaign conversation, between \
-            a podcast host and a guest about {qq[i + 1]}. The conversation has to be easy to understand for popular science communication. \
-            The host will not introduce or welcome the guest, rather the host will ask a question right from the beginning. \
-            End the conversation about this tipic so that so that the host can continue immediately the next topic of the conversation. \
-            AVOID LISTING NAMES.',1)
+            with open(f'./output/Conversation.txt', 'r') as file:
+        	    conver = file.read()
+            #conv = query(f'Based on the content, print out a continuation of the conversation in TEXT. Consequently, avoid redundancies. \
+            #The conversation has to be strictly about {qq[i + 1]}, between a podcast host and a guest. \
+            #The guest has to AVOID asking questions already asked in the TEXT below. \
+            #The host has to AVOID providing answers already provided in the TEXT below. \
+            #The conversation has to have four interventions from the host and four from the guest. \
+            #The conversation has to be easy to understand for popular science communication. \
+            #The host will not introduce or welcome the guest, rather the host will continue the conversation right from the beginning. \
+            #Do not end the conversation at the end. AVOID LISTING NAMES.\nTEXT:\n{conver}',1)
+
+            #conv = query(f'Continuing from the content provided, generate a follow-up to the conversation in the TEXT. \
+            #Ensure there are no repetitive elements in the dialogue. This conversation should solely revolve around {qq[i + 1]}, \
+            #featuring interactions between a podcast host and a guest. To enhance the discussion\'s depth, \
+            #ensure the guest refrains from rephrasing questions already posed in the TEXT. Similarly, \
+            #the host should avoid reiterating answers already given in the TEXT. Construct a well-balanced conversation \
+            #with four contributions from both the host and the guest. The dialogue should be presented in a manner easily comprehensible \
+            #for a wide audience, focusing on popular science communication. Please note that the host will bypass the introduction \
+            #and jump directly into the ongoing conversation. The prompt should not conclude the conversation prematurely, \
+            #and refrain from listing specific names. Refer to the provided TEXT for context:\n{conver}',1)
+
+            auxiliar = f'Imagine you\'re hosting a podcast episode focused on explaining scientific concepts to a broad audience. \
+Generate a continuation of the conversation of the TEXT enclosed in [] below. The host MUST to start a conversation mentioning that we already \
+talked about {qq[i]}, and now we want to delve into {qq[i+1]}. This segment MUST finish with the host\'s eagerness to delve deeper \
+into {qq[i+2]} in the upcoming part of the conversation. To avoid redundancies, \
+do not reapeat points from the TEXT enclosed in [] below:\nTEXT:\n[{conver}]'
+
+            print(auxiliar)
+
+            conv = query(auxiliar)
+
+            #conv = query(f'Imagine you\'re hosting a podcast episode focused on explaining scientific concepts to a broad audience. \
+            #The topic of this segment is {qq[i + 1]}. You\'re having a conversation with an expert guest to explore specific \
+            #aspects related to {qq[i + 1]}. In this segment, continue the conversation about {qq[i + 1]} with your guest. \
+            #Avoid repeating points from the provided TEXT, and instead, dive into new insights and information that will be \
+            #engaging and comprehensible for your listeners. As the host, you won\'t need to introduce the guest or welcome them, \
+            #since that\'s covered in the prior part of the episode. Also, remember not to finalize the conversation, as it\'s \
+            #just a part of the episode. Please provide a concise conversation about {qq[i + 1]} that includes at least two \
+            #interventions from both you as the host and your guest. The focus should be on presenting additional information, \
+            #explanations, or examples that build upon what was already introduced in the provided TEXT, all while maintaining \
+            #a popular science communication style. Conclude this segment by expressing the host\'s eagerness to delve deeper \
+            #into {qq[i+2]} in the upcoming part of the conversation. Here\'s the provided TEXT for your reference:\n{conver}',0)
             prGreen(f"We got topic {i + 1}")
             save_output("main.txt", f'{conv}\n\n')
-            save_output(f"Conversation.txt", f'{conv}\n\n   ####   DIVISION   ####\n\n')
+            save_output(f"Conversation.txt", f'{conv}\n\n   ####   DIVISION {i+1}  ####\n\n')
         subprocess.run(["open", f"./output/Conversation.txt"], check=True)
         prGreenIn(f"\nNOW REVIEW Conversation.txt AND PRESS ENTER TO CONTINUE")
     else:     
